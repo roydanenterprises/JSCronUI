e5e4465eadfabd986eed6a5771609d02380b53df
James McCollum <jmccollum@roydan.com>
2016-06-05 15:08:45 -0500

    Read encrypted file correctly

--- deploy.sh
+++ deploy.sh
@@ -54,7 +54,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa.enc -out id_rsa -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../id_rsa.enc -out id_rsa -d
 chmod 600 id_rsa
 eval `ssh-agent -s`
 ssh-add id_rsa
